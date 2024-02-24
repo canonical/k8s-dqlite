3fe5aff450b37ba66c299000cd441cd7db1a8f5c
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-24 14:32:27 +0200

    revert unused prefix manipulation in count

--- pkg/kine/logstructured/sqllog/sql.go
+++ pkg/kine/logstructured/sqllog/sql.go
@@ -472,10 +472,6 @@ func canSkipRevision(rev, skip int64, skipTime time.Time) bool {
 }
 
 func (s *SQLLog) Count(ctx context.Context, prefix string, revision int64) (int64, int64, error) {
-	if strings.HasSuffix(prefix, "/") {
-		prefix += "%"
-	}
-
 	if revision == 0 {
 		return s.d.CountCurrent(ctx, prefix)
 	}
