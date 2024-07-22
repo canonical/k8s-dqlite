fcdae605b3013b779b9758b40a6b57892cce05f3
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-07-22 10:50:56 +0200

    more renaming

--- pkg/kine/server/otel.go
+++ pkg/kine/server/otel.go
@@ -26,23 +26,23 @@ func init() {
 	otelTracer = otel.Tracer(otelName)
 	otelMeter = otel.Meter(otelName)
 
-	createCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.create", name), metric.WithDescription("Number of create requests"))
+	createCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.create", otelName), metric.WithDescription("Number of create requests"))
 	if err != nil {
 		logrus.WithError(err).Warning("Otel failed to create create counter")
 	}
-	deleteCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.delete", name), metric.WithDescription("Number of delete requests"))
+	deleteCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.delete", otelName), metric.WithDescription("Number of delete requests"))
 	if err != nil {
 		logrus.WithError(err).Warning("Otel failed to create delete counter")
 	}
-	getCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.get", name), metric.WithDescription("Number of get requests"))
+	getCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.get", otelName), metric.WithDescription("Number of get requests"))
 	if err != nil {
 		logrus.WithError(err).Warning("Otel failed to create get counter")
 	}
-	listCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.list", name), metric.WithDescription("Number of list requests"))
+	listCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.list", otelName), metric.WithDescription("Number of list requests"))
 	if err != nil {
 		logrus.WithError(err).Warning("Otel failed to create list counter")
 	}
-	updateCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.update", name), metric.WithDescription("Number of update requests"))
+	updateCnt, err = otelMeter.Int64Counter(fmt.Sprintf("%s.update", otelName), metric.WithDescription("Number of update requests"))
 	if err != nil {
 		logrus.WithError(err).Warning("Otel failed to create update counter")
 	}
