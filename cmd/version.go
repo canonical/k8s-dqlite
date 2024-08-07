4570b1ed3fb749e3192ac8e0d1b4d1dab9bdb987
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2024-08-07 16:20:01 +0200

    Build raft with dqlite (#153)

--- cmd/version.go
+++ cmd/version.go
@@ -7,7 +7,6 @@ import (
 
 /*
 #include "dqlite.h"
-#include "raft.h"
 #include "sqlite3.h"
 #include "uv.h"
 
@@ -26,16 +25,6 @@ void print_dqlite_library_versions() {
 		(dqlite_version_number() / 100) % 100,
 		dqlite_version_number() % 100
 	);
-
-	// TODO(neoaggelos): dynamic libraft does not have raft_version_number() symbol
-	// printf("raft: %d.%d.%d (compiled), %d.%d.%d (linked)\n",
-	// 	RAFT_VERSION_NUMBER / (100*100),
-	// 	(RAFT_VERSION_NUMBER / 100) % 100,
-	// 	RAFT_VERSION_NUMBER % 100,
-	// 	raft_version_number() / (100 * 100),
-	// 	(raft_version_number() / 100) % 100,
-	// 	raft_version_number() % 100
-	// );
 
 	printf("libuv: %d.%d.%d (compiled), %s (linked)\n",
 		UV_VERSION_MAJOR, UV_VERSION_MINOR, UV_VERSION_PATCH,
