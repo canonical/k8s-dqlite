76925d01dd0f99a0bf513d6ff41e1ca2acdb6815
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-09-24 14:47:08 +0200

    on key not found return success false

--- pkg/kine/server/delete.go
+++ pkg/kine/server/delete.go
@@ -61,9 +61,6 @@ func (l *LimitedServer) delete(ctx context.Context, key string, revision int64) 
 		if err != nil {
 			return nil, err
 		}
-		if kv == nil {
-			resp.Succeeded = true
-		}
 		resp.Responses = []*etcdserverpb.ResponseOp{
 			{
 				Response: &etcdserverpb.ResponseOp_ResponseRange{
