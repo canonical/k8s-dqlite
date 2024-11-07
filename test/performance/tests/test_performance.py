b0e0795c765a972b8a6a919aa3c54b29032022c1
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-11-07 17:51:47 +0100

    third node also cp

--- test/performance/tests/test_performance.py
+++ test/performance/tests/test_performance.py
@@ -4,26 +4,26 @@ import logging
 from typing import List
 
 import pytest
-from test_util import config, harness, util
+from test_util import harness, util
 
 LOG = logging.getLogger(__name__)
 
 
 @pytest.mark.node_count(3)
-def test_create_cluster(instances: List[harness.Instance]):
+def test_load_test(instances: List[harness.Instance]):
     cluster_node = instances[0]
-    joining_cp = instances[1]
-    joining_worker = instances[2]
+    joining_node = instances[1]
+    joining_node2 = instances[2]
 
-    join_token = util.get_join_token(cluster_node, joining_cp)
-    join_token_worker = util.get_join_token(cluster_node, joining_worker, "--worker")
-    util.join_cluster(joining_cp, join_token)
-    util.join_cluster(joining_worker, join_token_worker)
+    join_token = util.get_join_token(cluster_node, joining_node)
+    join_token2 = util.get_join_token(cluster_node, joining_node2)
+    util.join_cluster(joining_node, join_token)
+    util.join_cluster(joining_node2, join_token2)
 
     util.wait_until_k8s_ready(cluster_node, instances)
     nodes = util.ready_nodes(cluster_node)
     assert len(nodes) == 3, "nodes should have joined cluster"
 
     assert "control-plane" in util.get_local_node_status(cluster_node)
-    assert "control-plane" in util.get_local_node_status(joining_cp)
-    assert "worker" in util.get_local_node_status(joining_worker)
+    assert "control-plane" in util.get_local_node_status(joining_node)
+    assert "worker" in util.get_local_node_status(joining_node2)
