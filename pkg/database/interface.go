fa549e30257bfb08999cd1d6768cf510d22ffb5d
Marco Manino <mrcmnn@gmail.com>
2025-01-14 11:54:26 +0100

    Addressing review

--- pkg/database/interface.go
+++ pkg/database/interface.go
@@ -11,9 +11,7 @@ var errDBClosed = errors.New("sql: database is closed")
 type Interface interface {
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	ExecContext(ctx context.Context, query string, args ...any) (result sql.Result, err error)
-
 	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
-
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (Transaction, error)
 	Conn(ctx context.Context) (*sql.Conn, error)
 	Close() error
@@ -22,9 +20,7 @@ type Interface interface {
 type Transaction interface {
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	ExecContext(ctx context.Context, query string, args ...any) (result sql.Result, err error)
-
 	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
-
 	StmtContext(ctx context.Context, stmt *sql.Stmt) *sql.Stmt
 	Commit() error
 	Rollback() error
@@ -33,9 +29,7 @@ type Transaction interface {
 type Wrapped[T Transaction] interface {
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	ExecContext(ctx context.Context, query string, args ...any) (result sql.Result, err error)
-
 	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
-
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (T, error)
 	Conn(ctx context.Context) (*sql.Conn, error)
 	Close() error
--- pkg/database/prepared.go
+++ pkg/database/prepared.go
@@ -22,7 +22,7 @@ func init() {
 type preparedDb[T Transaction] struct {
 	underlying Wrapped[T]
 	mu         sync.RWMutex
-	store      map[string]*sql.Stmt
+	cache      map[string]*sql.Stmt
 }
 
 // NewPrepared creates a new Interface that wraps the given database and
@@ -32,7 +32,7 @@ type preparedDb[T Transaction] struct {
 func NewPrepared[T Transaction](db Wrapped[T]) Interface {
 	return &preparedDb[T]{
 		underlying: db,
-		store:      make(map[string]*sql.Stmt),
+		cache:      make(map[string]*sql.Stmt),
 	}
 }
 
@@ -76,7 +76,7 @@ func (db *preparedDb[T]) prepare(ctx context.Context, query string) (stmt *sql.S
 	}()
 
 	db.mu.RLock()
-	stmt = db.store[query]
+	stmt = db.cache[query]
 	db.mu.RUnlock()
 	if stmt != nil {
 		return stmt, nil
@@ -89,8 +89,10 @@ func (db *preparedDb[T]) prepare(ctx context.Context, query string) (stmt *sql.S
 		return nil, errDBClosed
 	}
 
-	// Check again if the query was prepared during locking
-	stmt = db.store[query]
+	// Given that some time has passed since the unlock of the read lock, and the lock of the
+	// write lock, another goroutine might have already prepared this query, so we should check
+	// again to avoid preparing the same query twice.
+	stmt = db.cache[query]
 	if stmt != nil {
 		return stmt, nil
 	}
@@ -99,7 +101,7 @@ func (db *preparedDb[T]) prepare(ctx context.Context, query string) (stmt *sql.S
 	if err != nil {
 		return nil, err
 	}
-	db.store[query] = prepared
+	db.cache[query] = prepared
 	return prepared, nil
 }
 
@@ -124,12 +126,12 @@ func (db *preparedDb[T]) Close() error {
 	defer db.mu.Unlock()
 
 	errs := []error{}
-	for _, stmt := range db.store {
+	for _, stmt := range db.cache {
 		if err := stmt.Close(); err != nil {
 			errs = append(errs, err)
 		}
 	}
-	db.store = nil
+	db.cache = nil
 
 	if err := db.underlying.Close(); err != nil {
 		errs = append(errs, err)
