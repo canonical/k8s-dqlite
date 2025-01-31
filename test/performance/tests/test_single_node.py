43ce0b06c277c025234861d4d77f1feb938b8c1d
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-01-31 13:16:43 +0000

    Continue the kube-burner loop even in case of failures
    We've been hitting Dqlite errors such as "database is locked".
    At the moment it's unclear if this is a transient error or if
    the database is compromised.
    
    To answer this, we'll make a small change to the perf test,
    resuming the kube-burner loop even in case of failures and
    propagating the exceptions after completing the loop.

--- test/performance/tests/test_single_node.py
+++ test/performance/tests/test_single_node.py
@@ -1,17 +1,31 @@
 #
 # Copyright 2025 Canonical, Ltd.
 #
+import logging
+
 from test_util import harness, metrics
+
+LOG = logging.getLogger(__name__)
 
 
 def test_single_node_load(session_instance: harness.Instance):
     """Test the performance of a single node cluster with all features enabled."""
     metrics.configure_kube_burner(session_instance)
     process_dict = metrics.collect_metrics([session_instance])
-    try:
-        for iteration in range(10):
+
+    raised_exc = None
+    for iteration in range(10):
+        try:
             metrics.run_kube_burner(session_instance)
-    finally:
-        # Collect the metrics even if kube-burner fails.
-        metrics.stop_metrics([session_instance], process_dict)
-        metrics.pull_metrics([session_instance], "single-node")
+        except Exception as ex:
+            LOG.exception("kube-burner failed, starting new iteration.")
+            # We'll start another iteration even if the test failed to see
+            # if it was a transient failure or if Dqlite was compromised.
+            raised_exc = ex
+
+    # Collect the metrics even if kube-burner fails.
+    metrics.stop_metrics([session_instance], process_dict)
+    metrics.pull_metrics([session_instance], "single-node")
+
+    if raised_exc:
+        raise raised_exc
