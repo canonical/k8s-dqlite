ec16b090c813a23761d9b61d9c69078ce8122e1f
Marco Manino <mrcmnn@gmail.com>
2025-02-14 12:32:50 +0100

    Enable dynamic trailing in tests as well

--- test/util_test.go
+++ test/util_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	dqlitedrv "github.com/canonical/go-dqlite/v3"
 	"github.com/canonical/go-dqlite/v3/app"
 	"github.com/canonical/k8s-dqlite/pkg/database"
 	"github.com/canonical/k8s-dqlite/pkg/instrument"
@@ -171,6 +172,11 @@ func startDqlite(ctx context.Context, tb testing.TB, dir string, listener *instr
 	app, err := app.New(dir,
 		app.WithAddress(listener.Address),
 		app.WithExternalConn(listener.Connect, listener.AcceptedConns),
+		app.WithSnapshotParams(dqlitedrv.SnapshotParams{
+			Threshold: 512,
+			Trailing:  4096,
+			Strategy:  dqlitedrv.TrailingStrategyDynamic,
+		}),
 	)
 	if err != nil {
 		tb.Fatalf("failed to create dqlite app: %v", err)
