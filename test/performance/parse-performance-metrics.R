58db3e698cf67de345634f26e3f00cf23e91ca99
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2025-02-28 09:18:35 +0200

    CI: performance test improvements (RSS plot, fixed scale, retry kube-burner download) (#262)

--- test/performance/parse-performance-metrics.R
+++ test/performance/parse-performance-metrics.R
@@ -44,6 +44,8 @@ for (file in metrics_files) {
   # Convert numeric columns and handle NA values
   df[2:(ncol(df) - 1)] <- lapply(df[2:(ncol(df) - 1)], function(x) as.numeric(as.character(x)))
   df[is.na(df)] <- 0
+  # Convert RSS kiB to MiB
+  df["RSS"] <- df["RSS"] / 1024
   
   metrics_data[[basename(file)]] <- df
 }
@@ -54,20 +56,22 @@ if (length(metrics_data) < 1) {
 }
 
 # Define function to create and save plots
-create_plot <- function(data_list, y_column, title, y_label, file_suffix) {
+create_plot <- function(data_list, y_column, title, y_label, file_suffix, max_y) {
   plot <- ggplot() +
     lapply(names(data_list), function(node) {
       geom_line(data = data_list[[node]], aes(x = 1:nrow(data_list[[node]]), y = !!sym(y_column), color = node))
     }) +
-    labs(title = title, x = "Time", y = y_label) 
-  
+    labs(title = title, x = "Time", y = y_label) +
+    ylim(c(0, max_y))
+
   # Save plot
   ggsave(filename = file.path(opt$out, paste0(file_suffix, ".png")), plot = plot, width = 8, height = 6)
 }
 
 print("Creating plots")
 # Generate and save all required plots
-create_plot(metrics_data, "X.CPU", "CPU Usage Over Time", "% CPU", "cpu_usage_plot")
-create_plot(metrics_data, "X.MEM", "Memory Usage Over Time", "% MEM", "mem_usage_plot")
-create_plot(metrics_data, "kB_rd_s", "IO Read Usage Over Time", "kB_rd_s", "io_read_usage_plot")
-create_plot(metrics_data, "kB_wr_s", "IO Write Usage Over Time", "kB_wr_s", "io_write_usage_plot")
+create_plot(metrics_data, "X.CPU", "CPU Usage Over Time", "% CPU", "cpu_usage_plot", 250)
+create_plot(metrics_data, "X.MEM", "Memory Usage Over Time", "% MEM", "mem_usage_plot", 2)
+create_plot(metrics_data, "RSS", "Resident Set Size in MiB Over Time", "RSS", "rss_plot", 400)
+create_plot(metrics_data, "kB_rd_s", "IO Read Usage Over Time", "kB_rd_s", "io_read_usage_plot", 30)
+create_plot(metrics_data, "kB_wr_s", "IO Write Usage Over Time", "kB_wr_s", "io_write_usage_plot", 35000)
