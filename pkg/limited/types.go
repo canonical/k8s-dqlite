63b737850606b2d94e1d5300cbbbfc2f2b64cae5
Reza Abbasalipour <reza.abbasalipour@canonical.com>
2025-04-10 13:02:39 +0400

    chore: remove backend interface
    Signed-off-by: Reza Abbasalipour <reza.abbasalipour@canonical.com>

--- pkg/limited/types.go
+++ pkg/limited/types.go
@@ -1,8 +1,6 @@
 package limited
 
 import (
-	"context"
-
 	"go.etcd.io/etcd/api/v3/mvccpb"
 	"go.etcd.io/etcd/api/v3/v3rpc/rpctypes"
 )
@@ -19,21 +17,6 @@ type CompactedError struct {
 func (e *CompactedError) Error() string { return ErrCompacted.Error() }
 
 func (e *CompactedError) Is(target error) bool { return target == ErrCompacted }
-
-type Backend interface {
-	Start(ctx context.Context) error
-	Stop() error
-	Create(ctx context.Context, key, value []byte, lease int64) (int64, bool, error)
-	Delete(ctx context.Context, key []byte, revision int64) (int64, bool, error)
-	List(ctx context.Context, key, rangeEnd []byte, limit, revision int64) (int64, []*KeyValue, error)
-	Count(ctx context.Context, key, rangeEnd []byte, revision int64) (int64, int64, error)
-	Update(ctx context.Context, key, value []byte, revision, lease int64) (int64, bool, error)
-	WatcherGroup(ctx context.Context) (WatcherGroup, error)
-	DbSize(ctx context.Context) (int64, error)
-	GetCompactRevision(ctx context.Context) (int64, int64, error)
-	DoCompact(ctx context.Context) error
-	Close() error
-}
 
 type WatcherGroup interface {
 	// Watch will add a watcher to the group. If startRevision is not 0, the first notification
