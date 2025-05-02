ff1b6ce41deb7a588541871c22d75951ba0a63d8
Louise K Schmidtgen <louise.schmidtgen@canonical.com>
2025-05-02 14:40:36 +0200

    comments

--- pkg/backend/v1/sqlite/schema_test.go
+++ pkg/backend/v1/sqlite/schema_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	sqlite "github.com/canonical/k8s-dqlite/pkg/backend/v1/sqlite"
+	"github.com/canonical/k8s-dqlite/pkg/backend/v1/sqlite"
 )
 
 func TestCanMigrate(t *testing.T) {
