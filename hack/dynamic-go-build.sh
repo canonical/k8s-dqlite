a95ac35fdda1d3d4449d0a1aaecf1590a523f4a7
Homayoon Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-08-14 10:49:38 +0400

    chore: Pass Go tags using environment variables (#316)
    Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

--- hack/dynamic-go-build.sh
+++ hack/dynamic-go-build.sh
@@ -4,7 +4,10 @@ DIR="$(realpath `dirname "${0}"`)"
 
 . "${DIR}/dynamic-dqlite.sh"
 
+# Default tags if TAGS environment variable is not set
+TAGS="${TAGS:-libsqlite3}"
+
 go build \
-  -tags libsqlite3 \
+  -tags "${TAGS}" \
   -ldflags '-s -w -extldflags "-Wl,-rpath,$ORIGIN/lib -Wl,-rpath,$ORIGIN/../lib"' \
   "${@}"
